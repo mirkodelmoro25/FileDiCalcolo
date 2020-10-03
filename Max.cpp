e4681101bc2b9118840de41341b76cee7744e05a
mirkodelmoro25 <48946689+mirkodelmoro25@users.noreply.github.com>
2020-10-03 14:27:06 +0200

    Create Max.cpp

---
+++ Max.cpp
@@ -0,0 +1,31 @@
+#include "Max.h"
+
+Max::Max() {
+    attach();
+}
+Max::~Max() {
+    detach();
+}
+ void Max::update() {
+    value = op();
+}
+ void Max::attach() {
+    subject->registerO(this);
+}
+ void Max::detach() {
+    subject->removeO(this);
+}
+ double Max::op() {
+    double max;
+    if (cells.size() != 0) {
+        max = (*(cells.begin()))->getValue();
+        for (auto it:cells){
+            if (it->getValue() > max)
+                max = it->getValue();
+        }
+    }
+    else
+        max = 0;
+    return max;
+
+}
