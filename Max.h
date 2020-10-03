7bbd0aeb38f4ed0df55cc53c2a1be25670600ced
mirkodelmoro25 <48946689+mirkodelmoro25@users.noreply.github.com>
2020-10-03 14:26:36 +0200

    Create Max.h

---
+++ Max.h
@@ -0,0 +1,22 @@
+#ifndef FILEDICALCOLO_MAX_H
+#define FILEDICALCOLO_MAX_H
+
+
+#include "Observer.h"
+#include "Functions.h"
+
+class Max : public Observer, Functions{
+public:
+    Max ();
+    ~Max();
+    virtual void update() override;
+    virtual void attach() override;
+    virtual void detach() override;
+    virtual double op() override;
+
+private:
+    Cells* subject;
+};
+
+
+#endif //FILEDICALCOLO_MAX_H
