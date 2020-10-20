ab3cc75ceb4b9447cdb17e4e7e3fa2b0f716417b
mirkodelmoro25 <48946689+mirkodelmoro25@users.noreply.github.com>
2020-10-20 15:49:29 +0200

    Update Cells.cpp

--- Cells.cpp
+++ Cells.cpp
@@ -1,17 +1,39 @@
 #include "Cells.h"
 
-Cells::Cells(int i, int j, double v) {
-    row=i;
-    column=j;
-    value=v;
+Cells::Cells(double v, int i, int j, QTableWidget* tableWidget) {
+    value = v;
+    QTextEdit* sCell = new QTextEdit();
+    sCell->setPlainText(QString::number(value));
+    tableWidget->setCellWidget(i, j, sCell);
+    t = sCell;
+   // QTableWidgetItem* q = new QTableWidgetItem("0");
+    //tableWidget->setItem(i,j,QString::number(v));
+    if (sumF)
+        registerO(dynamic_cast<Observer *>(sumF));
+    if (maxF)
+        registerO(dynamic_cast<Observer *>(maxF));
+    if (minF)
+        registerO(dynamic_cast<Observer *>(minF));
+    if (mediaF)
+        registerO(dynamic_cast<Observer *>(mediaF));
 }
 
+Cells::~Cells() noexcept {
+    if (sumF)
+        removeO(dynamic_cast<Observer *>(sumF));
+    if (maxF)
+        removeO(dynamic_cast<Observer *>(maxF));
+    if (minF)
+        removeO(dynamic_cast<Observer *>(minF));
+    if (mediaF)
+        removeO(dynamic_cast<Observer *>(mediaF));
+}
 void Cells::registerO (Observer* o) {
     observers.push_back(o);
 }
 void Cells::removeO (Observer* o) {
     observers.remove(o);
-};
+}
 void Cells::notify() const {
     for(auto itr = observers.begin(); observers.end() != itr; itr++) {
         (*itr)->update();
