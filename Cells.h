26337f9f194168497194cf6386f364b56144fc9f
mirkodelmoro25 <48946689+mirkodelmoro25@users.noreply.github.com>
2020-10-20 15:49:00 +0200

    Update Cells.h

--- Cells.h
+++ Cells.h
@@ -1,27 +1,44 @@
+//
+// Created by mirko25 on 18/09/20.
+//
+
 #ifndef FOGLIODICALCOLO_CELLS_H
 #define FOGLIODICALCOLO_CELLS_H
 #include "Subject.h"
 #include <list>
+#include "Sum.h"
+#include "Max.h"
+#include "Min.h"
+#include "Media.h"
+#include <QtWidgets/QTableWidget>
+#include <QtWidgets/QTextEdit>
+
 
 using namespace std;
 
-
 class Cells : public Subject {
 public:
-    Cells(int i=0, int j=0, double v=0);
-    virtual ~Cells() {}
-    void registerO (Observer* o) override;
-    void removeO (Observer* o) override;
+    Cells (double v, int i, int j, QTableWidget* tableWidget);
+    virtual ~Cells();
+    void registerO (Observer * o) override;
+    void removeO (Observer * o) override;
     void notify() const override;
     void setValue (double v);
-    double getValue () {return value;}
+    double getValue() { return value;}
+    QTextEdit* getCell() {return t;}
+
 private:
     list<Observer*> observers;
-    int row;
-    int column;
     double value;
+    Sum* sumF;
+    Max* maxF;
+    Min* minF;
+    Media* mediaF;
+    QTextEdit* t;
+
 };
 
 
 
 #endif //FOGLIODICALCOLO_CELLS_H
+
