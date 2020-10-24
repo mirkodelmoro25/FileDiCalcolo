715462efd577e27bf4a8fc1dd89fc4f482b05da6
mirkodelmoro25 <48946689+mirkodelmoro25@users.noreply.github.com>
2020-10-24 13:17:32 +0200

    Update Window.cpp

--- Window.cpp
+++ Window.cpp
@@ -12,8 +12,11 @@ Window::Window(QWidget* parent) :QMainWindow(parent) {
         }
     }
 
-    sum = new Sum(c, tableWidget);
-    
+    sum = new Sum(cc, tableWidget);
+    max = new Max(cc,tableWidget);
+    min = new Min(cc,tableWidget);
+    media = new Media(cc,tableWidget);
+
     this->setFixedSize(1030,325);
     this->setWindowTitle("Foglio di Calcolo");
 
@@ -42,8 +45,51 @@ Window::Window(QWidget* parent) :QMainWindow(parent) {
     mediaT->setReadOnly(true);
     tableWidget->setCellWidget(8,9,mediaT);
 
-    
+
     sumCell = new QTextEdit();
+    sumCell->setPlainText(QString::number(0));
+    tableWidget->setCellWidget(9, 6, sumCell);
+    sumCell->setReadOnly(true);
+    maxCell = new QTextEdit();
+    maxCell->setPlainText(QString::number(0));
+    tableWidget->setCellWidget(9, 7, maxCell);
+    maxCell->setReadOnly(true);
+    minCell = new QTextEdit();
+    minCell->setPlainText(QString::number(0));
+    tableWidget->setCellWidget(9, 8, minCell);
+    minCell->setReadOnly(true);
+    mediaCell = new QTextEdit();
+    mediaCell->setPlainText(QString::number(0));
+    tableWidget->setCellWidget(9, 9, mediaCell);
+    mediaCell->setReadOnly(true);
+
+    connect(tableWidget, SIGNAL(cellChanged(int,int )), this, SLOT(cellChanged(int,int)));
+
+}
+
+void Window::cellChanged(int row, int column) {
+    double s = tableWidget->item(row,column)->text().toDouble();
+
+    for (auto it : c){
+        if (it->getRow() == row && it->getColumn() == column && !it->getRegistered()){
+            sum->putCells(it);
+            max->putCells(it);
+            min->putCells(it);
+            media->putCells(it);
+            it->setRegistered(true);
+            it->registerO(sum);
+            it->registerO(max);
+            it->registerO(min);
+            it->registerO(media);
+            it->setValue(s);
+        }
+        else if (it->getRow() == row && it->getColumn() == column){
+            it->setValue(s);
+        }
+    }
+
+}
+
     sumCell->setPlainText(QString::number(0));
     tableWidget->setCellWidget(9, 6, sumCell);
     sumCell->setReadOnly(true);
