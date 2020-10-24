7b4579ff87c75582caedccc6961cf4ad8cb09310
mirkodelmoro25 <48946689+mirkodelmoro25@users.noreply.github.com>
2020-10-24 11:54:36 +0200

    Update Media.cpp

--- Media.cpp
+++ Media.cpp
@@ -1,29 +1,38 @@
+#include <utility>
 #include "Media.h"
+#include "Cells.h"
 
-Media::Media(Cells* s) {
-    subject = s;
+Media::Media(list <Cells*> s, QTableWidget* tableWidget) {
+    subjects = std::move(s);
+    this->tableWidget = tableWidget;
     attach();
 }
 Media::~Media() {
     detach();
 }
 void Media::update() {
-    value = op();
+    value = op(subjects);
+    QTextEdit* mediaC = new QTextEdit();
+    mediaC->setPlainText(QString::number(value));
+    tableWidget->setCellWidget(9, 9, mediaC);
 }
 void Media::attach() {
-    subject->registerO(this);
+    for (auto it:subjects) {
+        it->registerO(this);
+    }
 }
 void Media::detach() {
-    subject->removeO(this);
-}
-double Media::op() {
-    double media = 0;
-    int c = 0;
-    for (auto it:cells) {
-        media += it->getValue();
-        c++;
+    for (auto it:subjects) {
+        it->removeO(this);
     }
-    media = media/c;
+}
+double Media::op(list<Cells*> c) {
+    double media = 0;
+    int count = 0;
+    for (auto it:c) {
+        media += it->getValue();
+        count++;
+    }
+    media = media/count;
     return media;
-
 }
