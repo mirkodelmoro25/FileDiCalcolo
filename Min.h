62c55cb8825ba6c63a4894a42cef900283acba21
mirkodelmoro25 <48946689+mirkodelmoro25@users.noreply.github.com>
2020-10-24 11:41:18 +0200

    Update Min.h

--- Min.h
+++ Min.h
@@ -4,18 +4,22 @@
 
 #include "Observer.h"
 #include "Functions.h"
+#include <list>
+#include <QtWidgets/QTableWidget>
+class Cells;
 
 class Min : public Observer, Functions{
 public:
-    Min (Cells* s);
+    Min (list <Cells*> s, QTableWidget* tableWidget);
     ~Min();
     virtual void update() override;
     virtual void attach() override;
     virtual void detach() override;
-    virtual double op() override;
+    virtual double op(list<Cells*> c) override;
 
 private:
-    Cells* subject;
+    list<Cells*> subjects;
+    QTableWidget* tableWidget;
 };
 
 #endif //FILEDICALCOLO_MIN_H
