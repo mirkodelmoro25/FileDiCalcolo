7bbb81916701ec50a494b0d14b9097681b7b9ff7
mirkodelmoro25 <48946689+mirkodelmoro25@users.noreply.github.com>
2020-10-30 15:14:57 +0100

    Update Min.cpp

--- Min.cpp
+++ Min.cpp
@@ -1,6 +1,3 @@
-//
-// Created by mirko25 on 03/10/20.
-//
 #include <utility>
 #include "Min.h"
 #include "Cells.h"
@@ -10,34 +7,38 @@ Min::Min(list <Cells*> s, QTableWidget* tableWidget) {
     this->tableWidget = tableWidget;
     attach();
 }
+
 Min::~Min() {
     detach();
 }
+
 void Min::update() {
-    value = op(subjects);
     QTextEdit* minC = new QTextEdit();
-    minC->setPlainText(QString::number(value));
+    minC->setPlainText(QString::number(op(subjects)));
     tableWidget->setCellWidget(9, 8, minC);
 }
+
 void Min::attach() {
     for (auto it:subjects) {
         it->registerO(this);
     }
 }
+
 void Min::detach() {
     for (auto it:subjects) {
         it->removeO(this);
     }
 }
+
 void Min::putCells(Cells* cell) {
     subjects.push_back(cell);
 }
+
 double Min::op(list<Cells*> c) {
-    double min;
+    double min = 99999;
     for (auto it:c){
         if (it->getValue() <= min)
             min = it->getValue();
     }
     return min;
-
 }
