01f43812ab231d503541092f4d5a702dc72a4fbe
mirkodelmoro25 <48946689+mirkodelmoro25@users.noreply.github.com>
2020-10-30 15:11:10 +0100

    Update Cells.cpp

--- Cells.cpp
+++ Cells.cpp
@@ -6,33 +6,26 @@ Cells::Cells(double v, int i, int j, QTableWidget* tableWidget) {
     value = v;
     row = i;
     column = j;
-    QTableWidgetItem* q = new QTableWidgetItem("0");
-    q->setData(Qt::EditRole, value);
+    registered = false;
+    QTableWidgetItem* q = new QTableWidgetItem("");
     tableWidget->setItem(i,j,q);
-    
+
 }
 
-Cells::~Cells() noexcept {
-    if (sumF)
-        removeO(dynamic_cast<Observer *>(sumF));
-    if (maxF)
-        removeO(dynamic_cast<Observer *>(maxF));
-    if (minF)
-        removeO(dynamic_cast<Observer *>(minF));
-    if (mediaF)
-        removeO(dynamic_cast<Observer *>(mediaF));
-}
 void Cells::registerO (Observer* o) {
     observers.push_back(o);
 }
+
 void Cells::removeO (Observer* o) {
     observers.remove(o);
 }
+
 void Cells::notify() const {
     for(auto it: observers) {
         it->update();
     }
 }
+
 void Cells::setValue (double v) {
     value = v;
     notify();
