19dd7eda23540b95fda12cbfef3a7490b9de785d
mirkodelmoro25 <48946689+mirkodelmoro25@users.noreply.github.com>
2020-10-30 15:16:27 +0100

    Update Sum.h

--- Sum.h
+++ Sum.h
@@ -1,13 +1,10 @@
-
 #ifndef FILEDICALCOLO_SUM_H
 #define FILEDICALCOLO_SUM_H
-
 
 #include "Observer.h"
 #include "Functions.h"
 #include <list>
 #include <QtWidgets/QTableWidget>
-
 class Cells;
 
 class Sum :  public Observer, public Functions {
