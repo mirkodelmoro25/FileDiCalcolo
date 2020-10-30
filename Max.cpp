d670b9f535e9510ed6758cb3529d100fedc7e92c
mirkodelmoro25 <48946689+mirkodelmoro25@users.noreply.github.com>
2020-10-30 15:12:42 +0100

    Update Max.cpp

--- Max.cpp
+++ Max.cpp
@@ -8,29 +8,34 @@ Max::Max(list <Cells*> s, QTableWidget* tableWidget) {
     this->tableWidget = tableWidget;
     attach();
 }
+
 Max::~Max() {
     detach();
 }
+
  void Max::update() {
-     value = op(subjects);
      QTextEdit* maxC = new QTextEdit();
-     maxC->setPlainText(QString::number(value));
+     maxC->setPlainText(QString::number(op(subjects)));
      tableWidget->setCellWidget(9, 7, maxC);
 }
+
  void Max::attach() {
      for (auto it:subjects) {
          it->registerO(this);
      }
 }
+
  void Max::detach() {
      for (auto it:subjects) {
          it->removeO(this);
      }
 }
+
 void Max::putCells(Cells* cell) {
     subjects.push_back(cell);
 }
- double Max::op(list<Cells*> c) {
+
+double Max::op(list<Cells*> c) {
     double max;
     for (auto it:c){
         if (it->getValue() > max)
