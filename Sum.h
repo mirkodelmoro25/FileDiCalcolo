0d2404c894db90694dba1a91f6caa274549b92fd
mirkodelmoro25 <48946689+mirkodelmoro25@users.noreply.github.com>
2020-11-03 13:18:34 +0100

    bug limiti funzioni e eliminazione hard coded formule

--- Sum.h
+++ Sum.h
@@ -9,7 +9,7 @@ class Cells;
 
 class Sum :  public Observer, public Function{
 public:
-    Sum (list <Cells*> s, QTableWidget* tableWidget);
+    Sum (list <Cells*> s, QTableWidget* tableWidget, int x, int y);
     ~Sum();
     virtual void update() override;
     virtual void attach() override;
@@ -20,6 +20,7 @@ public:
 private:
     list<Cells*> subjects;
     QTableWidget* tableWidget;
+    int posX, posY;
 };
 
 #endif //FILEDICALCOLO_MIN_H
